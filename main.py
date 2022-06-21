b3939afe33cc8edab2d1b0face1e3cb509ced5df
srikar <ventrapragadasaisrikar123@gmail.com>
2022-06-21 14:48:23 +0530

    Basic Game environment

---
+++ main.py
@@ -0,0 +1,170 @@
+import pygame
+
+#initialization of all functions in pygame and mixer for the audio
+pygame.init()
+pygame.mixer.init() 
+
+#display stuff
+WIDTH = 893
+HEIGHT = 1000
+size = (WIDTH, HEIGHT)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Afloat")
+clock = pygame.time.Clock()
+FPS = 60 
+
+#intialization for game display the borders 
+WHITE = (255, 255, 255)
+GREY = (212, 210, 212)
+BLACK = (0, 0, 0)
+BLUE = (0, 97, 148)
+
+#no of balls and velocity of them 
+score = 0
+balls = 1
+velocity = 4 
+
+paddle_width = 54
+paddle_height = 20 
+wall_width = 16
+
+#sprite class used to combine image and rect/sounds/animations etc 
+#implies sprite object is a representation of a real object in the game 
+
+all_sprites_list = pygame.sprite.Group()
+
+paddle_sound = pygame.mixer.Sound('sounds/sounds_paddle.wav')
+wall_sound = pygame.mixer.Sound('sounds/sounds_wall.wav')
+
+class Paddle(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.rect = self.image.get_rect()
+
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+        if self.rect.x > WIDTH - wall_width - paddle_width:
+            self.rect.x = WIDTH - wall_width - paddle_width
+
+    def moveLeft(self, pixels):
+        self.rect.x -= pixels
+        if self.rect.x < wall_width:
+            self.rect.x = wall_width
+
+class Ball(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.rect = self.image.get_rect()
+        self.velocity = [velocity, velocity]
+
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+
+    def bounce(self):
+        self.velocity[0] = self.velocity[0]
+        self.velocity[1] = -self.velocity[1]
+
+#intialization of objects 
+paddle = Paddle(BLUE, paddle_width, paddle_height)
+paddle.rect.x = WIDTH // 2 - paddle_width // 2
+paddle.rect.y = HEIGHT - 65
+
+ball = Ball(WHITE, 10, 10)
+ball.rect.x = WIDTH // 2 - 5
+ball.rect.y = HEIGHT // 2 - 5 
+
+all_sprites_list.add(paddle)
+all_sprites_list.add(ball)
+
+
+def main(score, balls):
+
+    step = 0
+
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            paddle.moveLeft(10)
+        if keys[pygame.K_RIGHT]:
+            paddle.moveRight(10)
+
+        all_sprites_list.update()
+
+        if ball.rect.y < 40:
+            ball.velocity[1] = -ball.velocity[1]
+            wall_sound.play()
+
+        if ball.rect.x >= WIDTH - wall_width - 10:
+            ball.velocity[0] = -ball.velocity[0]
+            wall_sound.play()
+
+        if ball.rect.x <= wall_width:
+            ball.velocity[0] = -ball.velocity[0]
+            wall_sound.play()
+
+        if ball.rect.y > HEIGHT:
+            ball.rect.x = WIDTH // 2 - 5
+            ball.rect.y = HEIGHT // 2 - 5
+            ball.velocity[1] = ball.velocity[1]
+            balls += 1
+
+        if balls == 4:
+            font = pygame.font.Font('text/DSEG14Classic-Bold.ttf', 70)
+            text = font.render("GAME OVER", 1, WHITE)
+            text_rect = text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+            screen.blit(text, text_rect)
+            pygame.display.update()
+            pygame.time.wait(2000)
+            run = False
+
+        if pygame.sprite.collide_mask(ball, paddle):
+            ball.rect.x += ball.velocity[0]
+            ball.rect.y -= ball.velocity[1]
+            ball.bounce()
+            paddle_sound.play()
+
+    screen.fill(BLACK) 
+
+    
+
+    pygame.draw.line(screen, GREY, [0, 19], [WIDTH, 19], 40)
+    pygame.draw.line(screen, GREY, [(wall_width / 2) - 1, 0], [(wall_width / 2) - 1, HEIGHT], wall_width)
+    pygame.draw.line(screen, GREY, [(WIDTH - wall_width / 2) - 1, 0], [(WIDTH - wall_width / 2) - 1, HEIGHT], wall_width)
+
+    pygame.draw.line(screen, BLUE, [(wall_width / 2) - 1, HEIGHT - 65 + paddle_height / 2 - 54 / 2], [(wall_width / 2) - 1, HEIGHT - 65 + paddle_height / 2 - 54 / 2 + 54], wall_width)
+    pygame.draw.line(screen, BLUE, [(WIDTH - wall_width / 2) - 1, HEIGHT - 65 + paddle_height / 2 - 54 / 2], [(WIDTH - wall_width / 2) - 1, HEIGHT - 65 + paddle_height / 2 - 54 / 2 + 54], wall_width)
+
+
+    font = pygame.font.Font('text/DSEG14Classic-Bold.ttf', 70)
+    text = font.render(str(f"{score:03}"), 1, WHITE)
+    screen.blit(text, (80, 120))
+    text = font.render(str(balls), 1, WHITE)
+    screen.blit(text, (520, 41))
+    text = font.render('000', 1, WHITE)
+    screen.blit(text, (580, 120))
+    text = font.render('1', 1, WHITE)
+    screen.blit(text, (20, 40))
+
+    all_sprites_list.draw(screen)
+
+    pygame.display.update()
+
+    clock.tick(FPS)
+
+    pygame.quit()
+
+
+main(score, balls)
